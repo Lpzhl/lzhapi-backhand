9856492bc0347fbd2a41c2b4c9c549e066fd687a
lzh <2212384795@qq.com>
2024-05-04 00:01:53 +0800

    add 添加 lzhapi-gataway 网关模块

---
+++ lzhapi-getaway/src/main/java/com/lzh/lzhgetaway/LzhGetawayApplication.java
@@ -0,0 +1,13 @@
+package com.lzh.lzhgetaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzhGetawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzhGetawayApplication.class, args);
+    }
+
+}
---
+++ lzhapi-getaway/src/test/java/com/lzh/lzhgetaway/LzhGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzh.lzhgetaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzhGetawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
